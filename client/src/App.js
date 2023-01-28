2b3c75a5c18b3913d8ea6ae96b30abdbc02ba302
JustinSnyder611 <JustinSnyder611@gmail.com>
2023-01-27 19:57:34 -0600

    Fixed heroku Deployment

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider>
       <Router>
         <>
           <Navbar />
